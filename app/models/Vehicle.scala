6fad00f431b20b9f138ee8f78ac7a2b91172102e
kitaozao <kito_and@hotmail.com>
2016-10-20 18:50:32 -0200

    some class refactoring (#1)

--- app/models/Vehicle.scala
+++ app/models/Vehicle.scala
@@ -6,6 +6,7 @@ package models
 case class Vehicle(licensePlate: String,
                    city: City,
                    state: State,
+                   vehicleType: VehicleType,
                    brand: carBrand,
                    model: carModel,
                    degreeOfDamage: Degree
